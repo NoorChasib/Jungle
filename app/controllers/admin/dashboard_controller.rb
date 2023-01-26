2c4a461891089c6671f85f89182e15ce3f90d52f
Noor Chasib <noorchasib@gmail.com>
2023-01-26 11:28:06 -0800

    added missing admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  def show
-  end
+	http_basic_authenticate_with name: ENV['USER_NAME'],
+	                             password: ENV['PASS_WORD']
+
+	def show; end
 end
