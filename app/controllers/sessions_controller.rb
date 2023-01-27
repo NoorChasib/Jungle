e2f4a2402e1984d77c1a67cfd73a37a817c07643
Noor Chasib <noorchasib@gmail.com>
2023-01-27 09:55:37 -0800

    complete user authentication with styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,17 @@ class SessionsController < ApplicationController
 	def new; end
 
 	def create
-		user = User.find_by_email(params[:email])
-		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
+		@user = User.find_by_email(params[:email])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
 			redirect_to root_path
 		else
-			redirect_to :new
+			if !@user
+				@noemail = 'Email not found'
+			else
+				@user.errors.add(:password, 'is invalid')
+			end
+			render :new
 		end
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-	def new
-  end
+	def new; end
 
 	def create
-		user = User.new(user_params)
-		if user.save
-			session[:user_id] = user.id
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
 			redirect_to root_path
 		else
-			redirect_to signup_path
+			render :new
 		end
 	end
 
